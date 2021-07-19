3598c0bb1c4d24b88918d8d69628a018909c08a9
hawaijar <hawaijar.js@gmail.com>
2021-07-19 21:21:31 +0530

    Added DP/houseRobber

---
+++ Dynamic Programming/houseRobber.js
@@ -0,0 +1,20 @@
+/*
+ou are a professional robber planning to rob houses along a street. Each house has a certain amount of money stashed, the only constraint stopping you from robbing each of them is
+that adjacent houses have security systems connected and it will automatically contact the police if two adjacent houses
+were broken into on the same night.
+
+Given an integer array nums representing the amount of money of each house, return the maximum amount of money you can rob
+tonight without alerting the police.
+ */
+function rob(nums) {
+  function max(i, S, cache = {}) {
+    if (i < 0) return 0;
+    if (i === 0) return S[0];
+
+    if (i in cache) return cache[i];
+
+    return Math.max(S[i] + max(i - 2, S), max(i - 1, S));
+  }
+
+  return max(nums.length - 1, nums);
+}
